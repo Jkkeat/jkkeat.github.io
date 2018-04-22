976183089473725478c911f546cdeb78b3067058
Jkkeat <jkkeat@hotmail.com>
2018-04-22 11:10:22 +0800

    Initial Commit
    A001
    +Initial commit
    +Landing page

---
+++ index.js
@@ -0,0 +1,18 @@
+
+$(window).load(function()
+{
+    $l = $('.left')
+    $r = $('.right')
+    $l.mouseenter(function(){
+      $('.container').addClass('left-is-hovered');
+    })
+    .mouseleave(function() {
+      $('.container').removeClass('left-is-hovered');
+    });
+    
+    $r.mouseenter(function() {
+      $('.container').addClass('right-is-hovered');
+    }).mouseleave(function() {
+      $('.container').removeClass('right-is-hovered');
+    });
+});
