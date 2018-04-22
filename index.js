c3da37562be85c56d1c92eb0f0165181c9993e3c
Jkkeat <jkkeat@hotmail.com>
2018-04-22 12:30:24 +0800

    A002
    A002
    + Background video at landing page

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ $(window).load(function()
     $r = $('.right')
     $l.mouseenter(function(){
       $('.container').addClass('left-is-hovered');
+      let videoleft = document.getElementById("videoleft");
+      videoleft.play();
     })
     .mouseleave(function() {
       $('.container').removeClass('left-is-hovered');
+      let videoleft = document.getElementById("videoleft");
+      videoleft.pause();
     });
     
     $r.mouseenter(function() {
       $('.container').addClass('right-is-hovered');
+      let videoleft = document.getElementById("videoright");
+      videoleft.play();
     }).mouseleave(function() {
       $('.container').removeClass('right-is-hovered');
+      let videoleft = document.getElementById("videoright");
+      videoleft.pause();
     });
 });
